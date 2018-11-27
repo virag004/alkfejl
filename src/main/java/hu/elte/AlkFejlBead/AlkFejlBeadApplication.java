d5b2db1e6b97471bd19fb8aaae828993a57dd9fa
virag004 <virag004@freemail.hu>
2018-11-27 21:32:53 +0100

    springProject

---
+++ src/main/java/hu/elte/AlkFejlBead/AlkFejlBeadApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.AlkFejlBead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkFejlBeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkFejlBeadApplication.class, args);
+	}
+}
