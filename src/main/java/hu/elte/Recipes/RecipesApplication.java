0acdfaae12a109d5e5bb37b24743d120229aaa0b
virag004 <virag004@freemail.hu>
2018-12-21 22:04:04 +0100

    fixed conflict

--- src/main/java/hu/elte/Recipes/RecipesApplication.java
+++ src/main/java/hu/elte/Recipes/RecipesApplication.java
@@ -3,19 +3,6 @@ package hu.elte.Recipes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-@EnableAsync
-@EnableScheduling
-
 @SpringBootApplication
 public class RecipesApplication {
 
